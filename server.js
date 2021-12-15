69d10a2d9f4f7fdb98eb9d75810732599359b676
Vayva <vayva.85@inbox.ru>
2021-12-15 10:52:10 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,9 +3,17 @@ const fs = require('fs')
 
 const server = http.createServer((req, res) => {
 
-    const body = req.url === '/style.css'
-        ? fs.readFileSync('./public/style.css')
-        : fs.readFileSync('./public/index.html')
+    // const body = req.url === '/style.css'
+    //     ? fs.readFileSync('./public/style.css')
+    //     : fs.readFileSync('./public/index.html')
+    const publicPath = './public'
+    let body = null
+    try {
+        fs.readFileSync(`${publicPath}${req.url}`)
+    } catch (e) {
+        console.log(e)
+        body = fs.readFileSync(`${publicPath}/index.html`)
+    }
     res.end(body)
 })
 
